71d6ff3c26bf4f6d823b2ef8e435dcd2cd9d7441
melisa-kabasakal <meliskabasakal33@gmail.com>
2025-02-05 22:16:55 +0300

    final project

---
+++ src/main/java/com/workintech/zoo/entity/Kangaroo.java
@@ -0,0 +1,22 @@
+package com.workintech.zoo.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Kangaroo {
+    private Integer id;
+    private String name;
+    private Double height;
+    private Double weight;
+    private String gender;
+    private Boolean isAggressive;
+
+    public void setAggressive(boolean aggressive) {
+        isAggressive = aggressive;
+    }
+
+}
---
+++ src/main/java/com/workintech/zoo/exceptions/ZooGlobalExceptionHandle.java
@@ -0,0 +1,27 @@
+package com.workintech.zoo.exceptions;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class ZooGlobalExceptionHandle {
+    @ExceptionHandler
+    public ResponseEntity<ZooErrorResponse> handleException(ZooException exception){
+        ZooErrorResponse zooErrorResponse = new ZooErrorResponse(exception.getHttpStatus().value(), exception.getLocalizedMessage(), System.currentTimeMillis());
+        log.error("Exception occured: ", exception);
+        return new ResponseEntity<>(zooErrorResponse, exception.getHttpStatus());
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ZooErrorResponse> handleException(Exception exception){
+        ZooErrorResponse zooErrorResponse = new ZooErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(), exception.getLocalizedMessage(), System.currentTimeMillis());
+        log.error("Exception occured: ", exception);
+        return new ResponseEntity<>(zooErrorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+
+}
